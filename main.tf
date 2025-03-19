eb41774976f1704708769eb3a952a4ce86cc4e0f
Svenkatesan-New <svenkatesan17401@gmail.com>
2025-03-19 12:29:16 +0530

    chfx

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
   address_space = ["10.0.0.0/16"]
 }
-resource "azure_subnet" "example" {
+resource "azurerm_subnet" "example" {
   name ="example-subnet"
   resource_group_name = azurerm_resource_group.example.name
   virtual_network_name=azurerm_virtual_network.example.name
